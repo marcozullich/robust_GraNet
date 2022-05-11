9dbfa233a7b814325895f17fa607df2d735fb9b7
Marco Zullich <mzullich@login02.m100.cineca.it>
2022-05-11 15:02:16 +0200

    updatet stat utils for accuracy

--- stat_utils/accuracy_and_ttest.py
+++ stat_utils/accuracy_and_ttest.py
@@ -16,11 +16,15 @@ if __name__ == "__main__":
     parser.add_argument("--string_accuracy1", type=str, default="Test |  Acc:", help="String to search for accuracy")
     parser.add_argument("--accuracy_marker1", type=str, default="Acc:", help="Field marking accuracy")
     parser.add_argument("--field_separator1", type=str, default=" ", help="Field separator")
+    parser.add_argument("--index_from_marker1", type=int, default=None, help="Number of chars from accuracy_marker where the accuracy float value begins. Alternative if --field_speparator is not useful.")
+    parser.add_argument("--accuracy_field_length1", type=int, default=None, help="Length of the accuracy field. Used only when --index_from_marker is not None")
     parser.add_argument("--logs_folder2", type=str, required=True, help="Folder containing the logs")
     parser.add_argument("--log_file_pattern2", type=str, default=None, help="Pattern for the log files")
     parser.add_argument("--string_accuracy2", type=str, default=None, help="String to search for accuracy")
     parser.add_argument("--accuracy_marker2", type=str, default=None, help="Field marking accuracy")
     parser.add_argument("--field_separator2", type=str, default=None, help="Field separator")
+    parser.add_argument("--index_from_marker2", type=int, default=None, help="Number of chars from accuracy_marker where the accuracy float value begins. Alternative if --field_speparator is not useful.")
+    parser.add_argument("--accuracy_field_length2", type=int, default=None, help="Length of the accuracy field. Used only when --index_from_marker is not None")
     args = parser.parse_args()
 
     if args.log_file_pattern2 is None:
@@ -31,9 +35,13 @@ if __name__ == "__main__":
         args.accuracy_marker2 = args.accuracy_marker1
     if args.field_separator2 is None:
         args.field_separator2 = args.field_separator1
+    if args.index_from_marker2 is None:
+        args.index_from_marker2 = args.index_from_marker1
+    if args.accuracy_field_length2 is None:
+        args.accuracy_field_length2 = args.accuracy_field_length1
     
-    accuracies1 = np.array(get_accuracy(args.log_file_pattern1, args.logs_folder1, args.string_accuracy1, args.accuracy_marker1, args.field_separator1))
-    accuracies2 = np.array(get_accuracy(args.log_file_pattern2, args.logs_folder2, args.string_accuracy2, args.accuracy_marker2, args.field_separator2))
+    accuracies1 = np.array(get_accuracy(args.log_file_pattern1, args.logs_folder1, args.string_accuracy1, args.accuracy_marker1, args.field_separator1, args.index_from_marker1, args.accuracy_field_length1))
+    accuracies2 = np.array(get_accuracy(args.log_file_pattern2, args.logs_folder2, args.string_accuracy2, args.accuracy_marker2, args.field_separator2, args.index_from_marker2, args.accuracy_field_length2))
     
     print("Accuracy for folder 1", mean(accuracies1))
     print("Accuracy for folder 2", mean(accuracies2))
--- stat_utils/get_accuracy.py
+++ stat_utils/get_accuracy.py
@@ -10,16 +10,26 @@ def match_row_file(pattern, file_stream:io.StringIO):
         if re.match(pattern, line):
             return line
 
-def get_accuracy_from_row(row:str, accuracy_marker:str, field_separator:str):
-    fields = row.split(field_separator)
-    try:
-        accuracy_index = fields.index(accuracy_marker)
-        accuracy = float(fields[accuracy_index + 1])
-        return accuracy
-    except ValueError:
-        pass
+def get_accuracy_from_row(row:str, accuracy_marker:str, field_separator:str, index_from_marker:int=None, field_length:int=None):
+    if index_from_marker is None:
+        fields = row.split(field_separator)
+        try:
+            accuracy_index = fields.index(accuracy_marker)
+            accuracy = float(fields[accuracy_index + 1])
+            return accuracy
+        except ValueError:
+            pass
+    else:
+        try:
+            assert field_length is not None and field_length > 0, f"if index_from_marker specified, field_length cannot be 0 or None"
+            accuracy_index = row.index(accuracy_marker)
+            accuracy = float(row[accuracy_index + index_from_marker : accuracy_index + index_from_marker + field_length])
+            return accuracy
+        except ValueError:
+            pass
 
-def main(log_file_pattern, logs_folder, string_accuracy, accuracy_marker, field_separator):
+
+def main(log_file_pattern, logs_folder, string_accuracy, accuracy_marker, field_separator, chars_from_marker, field_length):
     log_files = [os.path.join(logs_folder, f) for f in os.listdir(logs_folder) if re.match(log_file_pattern, f)]
 
     accuracies = []
@@ -27,7 +37,7 @@ def main(log_file_pattern, logs_folder, string_accuracy, accuracy_marker, field_
         with open(log_file, "r") as f:
             row = match_row_file(string_accuracy, f)
             if row is not None:
-                accuracy = get_accuracy_from_row(row, accuracy_marker, field_separator)
+                accuracy = get_accuracy_from_row(row, accuracy_marker, field_separator, chars_from_marker, field_length)
                 accuracies.append(float(accuracy))
     
     return accuracies
@@ -39,7 +49,10 @@ if __name__ == "__main__":
     parser.add_argument("--string_accuracy", type=str, default="Test |  Acc:", help="String to search for accuracy")
     parser.add_argument("--accuracy_marker", type=str, default="Acc:", help="Field marking accuracy")
     parser.add_argument("--field_separator", type=str, default=" ", help="Field separator")
+    parser.add_argument("--index_from_marker", type=int, default=None, help="Number of chars from accuracy_marker where the accuracy float value begins. Alternative if --field_speparator is not useful.")
+    parser.add_argument("--accuracy_field_length", type=int, default=None, help="Length of the accuracy field. Used only when --index_from_marker is not None")
     parser.add_argument("--debug", action="store_true", help="Print debug information")
+    
     args = parser.parse_args()
 
     if args.debug:
@@ -51,7 +64,7 @@ if __name__ == "__main__":
     log_file_pattern = args.log_file_pattern
     logs_folder = os.path.expanduser(os.path.expandvars(args.logs_folder))
 
-    accuracies = main(log_file_pattern, logs_folder, args.string_accuracy, args.accuracy_marker, args.field_separator)
+    accuracies = main(log_file_pattern, logs_folder, args.string_accuracy, args.accuracy_marker, args.field_separator, args.index_from_marker, args.accuracy_field_length)
     print(accuracies)
     print(f"Mean: {statistics.mean(accuracies)}")
 
